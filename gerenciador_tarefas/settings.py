fc67643fdd44f549176227f6e7cf1b9d4c9b228e
KaioLF <kaiolimafernandes@gmail.com>
2024-09-12 18:00:37 -0300

    commit da settings

--- gerenciador_tarefas/settings.py
+++ gerenciador_tarefas/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gerenciador_tarefas_db',
         'USER': 'root',
-        'PASSWORD': 'nova_senha',
+        'PASSWORD': 'kaio42root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
